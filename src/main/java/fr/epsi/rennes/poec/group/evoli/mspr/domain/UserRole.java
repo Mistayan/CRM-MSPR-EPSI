4e50524b7bf9bdcc286246cec7e647bf1f187b85
Mistayan <helixmastaz@gmail.com>
2022-04-28 18:08:55 +0200

    push initial
    Roles Utilisateur (user, admin general, admin add, ...)
    
    De nouveaux roles sont a definir.?

--- src/main/java/fr/epsi/rennes/poec/group/evoli/mspr/domain/UserRole.java
+++ src/main/java/fr/epsi/rennes/poec/group/evoli/mspr/domain/UserRole.java
@@ -3,7 +3,6 @@ package fr.epsi.rennes.poec.group.evoli.mspr.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum UserRole implements GrantedAuthority {
-
     ROLE_USER, ROLE_ADMIN;
 
     @Override
