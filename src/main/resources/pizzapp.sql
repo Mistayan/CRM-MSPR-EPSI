ac18073a89a56dca4c395e4c38e80eb2b7b4b50a
Mistayan <Helixmastaz@gmail.com>
2022-05-18 15:58:45 +0200

    SQL update.
    Last = Order, user_order, order_articles

--- src/main/resources/pizzapp.sql
+++ src/main/resources/pizzapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 17 mai 2022 à 17:43
+-- Généré le : mer. 18 mai 2022 à 13:57
 -- Version du serveur : 8.0.27
 -- Version de PHP : 7.4.26
 
@@ -26,21 +26,6 @@ USE `pizzapp`;
 -- --------------------------------------------------------
 
 --
--- Structure de la table `commande_has_article`
---
--- Création : mar. 17 mai 2022 à 07:06
--- Dernière modification : mar. 17 mai 2022 à 07:06
---
-
-DROP TABLE IF EXISTS order_has_article;
-CREATE TABLE IF NOT EXISTS `commande_has_article` (
-  `commande_id` int DEFAULT NULL,
-  `pizza_id` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `ingredients`
 --
 -- Création : sam. 14 mai 2022 à 13:32
@@ -54,8 +39,13 @@ CREATE TABLE IF NOT EXISTS `ingredients` (
   `nb_calories` int NOT NULL,
   `prix` double NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
 
+--
+-- Tronquer la table avant d'insérer `ingredients`
+--
+
+TRUNCATE TABLE `ingredients`;
 --
 -- Déchargement des données de la table `ingredients`
 --
@@ -78,46 +68,87 @@ INSERT INTO `ingredients` (`id`, `type`, `label`, `nb_calories`, `prix`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `order`
+-- Structure de la table `order_`
 --
--- Création : lun. 16 mai 2022 à 22:27
+-- Création : mar. 17 mai 2022 à 22:03
+-- Dernière modification : mer. 18 mai 2022 à 07:43
 --
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE IF NOT EXISTS `order` (
+DROP TABLE IF EXISTS `order_`;
+CREATE TABLE IF NOT EXISTS `order_` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `mail` varchar(120) COLLATE utf8_bin NOT NULL,
-  `commande_uuid` varchar(128) COLLATE utf8_bin NOT NULL,
   `TVA` double NOT NULL,
   `prix_ttc` double NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
 
 --
--- Déchargement des données de la table `order`
+-- Tronquer la table avant d'insérer `order_`
 --
 
-INSERT INTO `order` (`id`, `mail`, `commande_uuid`, `TVA`, `prix_ttc`, `date`) VALUES
-(4, 'gael', '{\"pizza\": \"Margharita\",\"prix\": \"5.63\",\"pizza\": \"Margharita\",\"prix\": \"5.63\",\"pizza\": \"4Fromages\",\"prix\": \"4.94\",\"pizza\": \"Marghar', 2.1483886255924176, 41.209999999999994, '2022-05-17 14:33:38'),
-(5, 'gael', '{Margharita\",Royale}', 0.7986729857819892, 15.32, '2022-05-17 14:35:31');
+TRUNCATE TABLE `order_`;
+--
+-- Déchargement des données de la table `order_`
+--
+
+INSERT INTO `order_` (`id`, `mail`, `TVA`, `prix_ttc`, `date`) VALUES
+(12, 'gael', 0.94, 17.96, '2022-05-18 10:20:08'),
+(13, 'gael', 0.94, 17.96, '2022-05-18 10:28:14'),
+(14, 'gael', 0.94, 17.96, '2022-05-18 10:29:00'),
+(15, 'gael', 0.94, 17.96, '2022-05-18 10:33:46'),
+(16, 'gael', 0.94, 17.96, '2022-05-18 11:20:51'),
+(17, 'gael', 0.94, 17.96, '2022-05-18 11:33:17'),
+(18, 'gael', 0.94, 17.96, '2022-05-18 11:33:50'),
+(19, 'gael', 0.94, 17.96, '2022-05-18 11:40:32'),
+(20, 'gael', 0.94, 17.96, '2022-05-18 11:44:32'),
+(21, 'gael', 0.94, 17.96, '2022-05-18 11:45:35'),
+(22, 'gael', 0.94, 17.96, '2022-05-18 11:46:48'),
+(23, 'gael', 0.94, 17.96, '2022-05-18 11:47:54');
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `order_articles`
 --
--- Création : mar. 17 mai 2022 à 17:36
+-- Création : mer. 18 mai 2022 à 09:42
 --
 
 DROP TABLE IF EXISTS `order_articles`;
 CREATE TABLE IF NOT EXISTS `order_articles` (
   `order_id` int NOT NULL,
-  `pizza_id` int NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`order_id`,`pizza_id`),
-  KEY `idx_order_date` (`date`)
+  `pizza_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+--
+-- Tronquer la table avant d'insérer `order_articles`
+--
+
+TRUNCATE TABLE `order_articles`;
+--
+-- Déchargement des données de la table `order_articles`
+--
+
+INSERT INTO `order_articles` (`order_id`, `pizza_id`) VALUES
+(14, 4),
+(14, 2),
+(15, 4),
+(15, 2),
+(16, 4),
+(16, 2),
+(18, 4),
+(18, 2),
+(19, 4),
+(19, 2),
+(20, 4),
+(20, 2),
+(21, 4),
+(21, 2),
+(22, 4),
+(22, 2),
+(23, 4),
+(23, 2);
 
 -- --------------------------------------------------------
 
@@ -132,8 +163,13 @@ CREATE TABLE IF NOT EXISTS `panier` (
   `id` int NOT NULL AUTO_INCREMENT,
   `timestamp` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
 
+--
+-- Tronquer la table avant d'insérer `panier`
+--
+
+TRUNCATE TABLE `panier`;
 --
 -- Déchargement des données de la table `panier`
 --
@@ -149,7 +185,9 @@ INSERT INTO `panier` (`id`, `timestamp`) VALUES
 (8, '2022-05-17T09:10:01.8199952'),
 (9, '2022-05-17T09:10:02.757614'),
 (10, '2022-05-17T09:10:04.7295788'),
-(11, '2022-05-17T09:10:05.5001671');
+(11, '2022-05-17T09:10:05.5001671'),
+(12, '2022-05-18T09:16:11.5797325'),
+(13, '2022-05-18T09:16:12.6625577');
 
 -- --------------------------------------------------------
 
@@ -157,15 +195,21 @@ INSERT INTO `panier` (`id`, `timestamp`) VALUES
 -- Structure de la table `panier_pizza`
 --
 -- Création : sam. 14 mai 2022 à 18:37
--- Dernière modification : dim. 15 mai 2022 à 23:18
+-- Dernière modification : mar. 17 mai 2022 à 19:49
+-- Dernière vérification : lun. 16 mai 2022 à 21:13
 --
 
 DROP TABLE IF EXISTS `panier_pizza`;
 CREATE TABLE IF NOT EXISTS `panier_pizza` (
   `panier_id` int NOT NULL,
   `pizza_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
 
+--
+-- Tronquer la table avant d'insérer `panier_pizza`
+--
+
+TRUNCATE TABLE `panier_pizza`;
 --
 -- Déchargement des données de la table `panier_pizza`
 --
@@ -179,10 +223,17 @@ INSERT INTO `panier_pizza` (`panier_id`, `pizza_id`) VALUES
 (4, 3),
 (5, 3),
 (6, 1),
+(11, 3),
 (10, 3),
 (9, 3),
 (8, 2),
-(7, 1);
+(7, 1),
+(11, 3),
+(11, 3),
+(11, 1),
+(11, 2),
+(11, 4),
+(12, 2);
 
 -- --------------------------------------------------------
 
@@ -197,8 +248,13 @@ CREATE TABLE IF NOT EXISTS `pizza` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `label` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
 
+--
+-- Tronquer la table avant d'insérer `pizza`
+--
+
+TRUNCATE TABLE `pizza`;
 --
 -- Déchargement des données de la table `pizza`
 --
@@ -224,6 +280,11 @@ CREATE TABLE IF NOT EXISTS `pizza_ingredient` (
   `ingredient_id` int UNSIGNED NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
 
+--
+-- Tronquer la table avant d'insérer `pizza_ingredient`
+--
+
+TRUNCATE TABLE `pizza_ingredient`;
 --
 -- Déchargement des données de la table `pizza_ingredient`
 --
@@ -276,6 +337,11 @@ CREATE TABLE IF NOT EXISTS `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin COMMENT='utilisateurs';
 
 --
+-- Tronquer la table avant d'insérer `user`
+--
+
+TRUNCATE TABLE `user`;
+--
 -- Déchargement des données de la table `user`
 --
 
@@ -283,6 +349,32 @@ INSERT INTO `user` (`id`, `email`, `password`, `userRole`) VALUES
 (1, 'user', '$2a$10$RDilYQ7Mi8VsN/wV2jmB1O.METkqETxjzLrNMX9s/ebZQHFGhCp2G', 'ROLE_ADMIN'),
 (5, 'user2', '$2a$10$uJcfmMWygag4y5Bm2o8I/OgmpYUJldIm6nJtBfadaBJvIpveXA3ba', 'ROLE_USER'),
 (8, 'gael', '$2a$10$.fXcSDgRbzZUIk.ZC5td9u.Jxt460Nms6gKnKRY4Kdcz2DarcUbHy', 'ROLE_USER');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_order`
+--
+-- Création : mer. 18 mai 2022 à 10:32
+--
+
+DROP TABLE IF EXISTS `user_order`;
+CREATE TABLE IF NOT EXISTS `user_order` (
+  `user_name` varchar(150) COLLATE utf8_bin DEFAULT NULL,
+  `order_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_bin;
+
+--
+-- Tronquer la table avant d'insérer `user_order`
+--
+
+TRUNCATE TABLE `user_order`;
+--
+-- Déchargement des données de la table `user_order`
+--
+
+INSERT INTO `user_order` (`user_name`, `order_id`) VALUES
+('gael', 23);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
