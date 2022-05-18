917e9ab0a0f9a236e6d76d75e6053292b065ee88
Mistayan <Helixmastaz@gmail.com>
2022-05-18 15:54:48 +0200

    Spring App configuration . iml
    now includes libs
    sources and excludes

--- src/main/webapp/js/nav.js
+++ src/main/webapp/js/nav.js
@@ -1,7 +1,7 @@
-const nav = new Vue ({
+const nav = new Vue({
     el: '#app',
     mounted() {
-        if (window.localStorage.getItem('nav') !== ''){
+        if (window.localStorage.getItem('nav')) {
             let link = window.localStorage.getItem('nav');
             localStorage.removeItem('nav')
             window.location.replace(link);
