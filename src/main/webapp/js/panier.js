880875ac2cafa4cad7774cb2a975dbe3c96e3fe1
Mistayan <Helixmastaz@gmail.com>
2022-05-24 00:14:49 +0200

    cleanup

--- src/main/webapp/js/panier.js
+++ src/main/webapp/js/panier.js
@@ -2,7 +2,7 @@
 // Redis BDD
 
 const app = new Vue({
-    el: '#app',
+    el: '#panier',
     data() { //le modèle de données
         return {
             pizzas: [],
@@ -13,33 +13,40 @@ const app = new Vue({
     },
     mounted() {
         // Actions au chargement de la page
-        this.panierId = window.localStorage.getItem('panier.id');
+        this.panierId = window.localStorage.getItem('panierId');
         if (this.panierId === null) {
             this.panierId = -1;
         }
         axios.get("/public/pizza")
             .then(response => {
-                this.pizzas = response.data.data
+                this.pizzas = response.data.data;
             });
         axios.get('/public/panier?panierId=' + this.panierId)
             .then(response => {
                 this.panier = response.data.data;
-                if (this.panier)
+                if (this.panier) {
                     this.panier.totalPrix = this.panier.totalPrix.toFixed(2);
+                    this.panierId = this.panier.id;
+                    }
             });
     },
     methods: { // Methodes interactives
         setPanier(response) {
             this.panier = response.data.data;
-            if (this.panier)
+            if (this.panier){
                 this.panier.totalPrix = this.panier.totalPrix.toFixed(2);
+                if (this.panier.id){
+                    this.panierId = this.panier.id
+                }
+            }
+
         },
         ajouterPizza(pizzaId) {
             <!--Charger panier-->
             axios.post('/public/panier/pizza?panierId=' + this.panierId + '&pizzaId=' + pizzaId + '&action=1')
                 .then(response => {
                     if (response.data.success) {
-                        localStorage.setItem('panier.id', response.data.data);
+                        localStorage.setItem('panierId', response.data.data);
                         <!-- Re-Charger panier après ajout-->
                         axios.get('/public/panier?panierId=' + response.data.data)
                             .then(response => {
@@ -49,10 +56,8 @@ const app = new Vue({
                 });
         },
         panierContainsPizza() {
-            if (this.panier && this.panier.pizzas && this.panier.pizzas.length > 0) {
-                return true
-            }
-            return false
+            return !!(this.panier && this.panier.pizzas && this.panier.pizzas.length > 0);
+
         },
         pizzaInPanier(pizza_) {
             if (this.countPizzaInCart(pizza_.id) > 0) {
@@ -61,7 +66,7 @@ const app = new Vue({
         },
         countPizzaInCart(pizzaId) {
             if (!this.panier || !this.panier.pizzas)
-                return 0;
+                return 0
             let i = 0;
             let count = 0;
             while (i < this.panier.pizzas.length) {
@@ -74,7 +79,7 @@ const app = new Vue({
             return count;
         },
         calc_price_pizza_panier(pizza) {
-            return pizza.prix * this.countPizzaInCart(pizza.id);
+            return (pizza.prix * this.countPizzaInCart(pizza.id)).toFixed(2);
         },
         enleverPizza(pizzaId) {
             <!-- supprimer pizza du panier -->
@@ -85,7 +90,7 @@ const app = new Vue({
                 .then(response => {
                     if (response.data.success) {
                         <!-- Actualiser le localstorage -->
-                        localStorage.setItem('panier.id', response.data.data);
+                        localStorage.setItem('panierId', response.data.data);
                         <!-- Re-Charger panier après suppression -->
                         axios.get('/public/panier?panierId=' + response.data.data)
                             .then(response => {
@@ -126,7 +131,7 @@ const app = new Vue({
             if (!localStorage.getItem('alconf')) {
                 <!-- Confirmer la commande-->
                 confirmAction = confirm("votre commande vous satisfait ?");
-                localStorage.setItem('alconf', '42')
+                localStorage.setItem('alconf', '117')
             } else {
                 confirmAction = true;
             }
