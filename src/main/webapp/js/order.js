5d936f0c99e55eefa7f9f93de00549626a351db4
Mistayan <Helixmastaz@gmail.com>
2022-05-25 01:04:06 +0200

    user can now see his cart (WIP)

--- src/main/webapp/js/order.js
+++ src/main/webapp/js/order.js
@@ -1,24 +1,46 @@
-const orders = new Vue({
-    el: '#orders',
+const order = new Vue({
+    el: '#order',
     data() { //le modèle de données
         return {
-            orders: []
+            orders: [],
+            pizzasRepo: [],
+            totalPizza: 0,
+            totalPrixHT: 0,
+            totalPrixTTC: 0,
+            totalCalories: 0
         }
     },
     mounted() {
         // Actions au chargement de la page
-        this.panierId = window.localStorage.getItem('panier.id');
-        if (this.panierId === null) {
-            this.panierId = -1;
-        }
         axios.get("/user/orders")
             .then(response => {
-                this.orders = response.data.data
+                    this.orders = Object(response.data.data)
+                    let i = 0;
+                    while (i < this.orders.length) {
+                        let order = this.orders.at(i)
+                        this.totalPrixTTC += order.prixTTC;
+                        this.totalPizza += order.pizzas.length;
+                        this.totalPrixHT += order.prixHT;
+                        i++
+                    }
                 }
             );
-        methods: {
-
-        }
-
+        axios.get("/public/pizza")
+            .then(response => {
+                this.pizzasRepo = response.data.data
+            });
+    },
+    methods: {
+        // countPizzaInOrder(pizzaId){
+        //     let count = 0
+        //     for (let _pizza in Object(this.orders.pizzas)){
+        //         if (_pizza.id === pizzaId)
+        //             count += 1
+        //     }
+        //     return count
+        // },
+        // calc_price_pizzas_order(pizza) {
+        //     return (pizza.prix * this.countPizzaInOrder(pizza.id)).toFixed(2)
+        // }
     }
 })
