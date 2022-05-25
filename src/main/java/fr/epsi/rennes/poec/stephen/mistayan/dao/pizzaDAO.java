8ace91afdd155768b9a9c9c0ae758b11eca9ba44
Mistayan <Helixmastaz@gmail.com>
2022-05-26 01:46:06 +0200

    Uniformizing connection / PS declaration

--- src/main/java/fr/epsi/rennes/poec/stephen/mistayan/dao/pizzaDAO.java
+++ src/main/java/fr/epsi/rennes/poec/stephen/mistayan/dao/pizzaDAO.java
@@ -4,13 +4,12 @@ import fr.epsi.rennes.poec.stephen.mistayan.domain.Ingredient;
 import fr.epsi.rennes.poec.stephen.mistayan.domain.Pizza;
 import fr.epsi.rennes.poec.stephen.mistayan.exception.TechnicalException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,11 +37,13 @@ public class pizzaDAO {
     public int createPizza(String label) throws TechnicalException {
         String sql = "INSERT INTO pizza (label) values (?)";
 
-        try (PreparedStatement stmt = ds.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            stmt.setString(1, label);
-            int ctrl = stmt.executeUpdate();
+        try (Connection conn = ds.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, label);
+            int ctrl = ps.executeUpdate();
             if (ctrl != 1) throw new SQLException("Error while inserting pizza");
-            ResultSet rs = stmt.getGeneratedKeys();
+            ResultSet rs = ps.getGeneratedKeys();
+            conn.close();
             if (rs.next()) {
                 return rs.getInt(1);
             }
@@ -55,11 +56,12 @@ public class pizzaDAO {
     public void addIngredientToPizza(int pizzaId, int ingredientId) throws TechnicalException {
         String sql = "INSERT INTO pizza_ingredient (pizza_id, ingredient_id) values (?, ?)";
 
-        try (PreparedStatement stmt = ds.getConnection().prepareStatement(sql)) {
-            stmt.setInt(1, pizzaId);
-            stmt.setInt(2, ingredientId);
-            int ctrl = stmt.executeUpdate();
-            //if (ctrl != 1) throw new BusinessException(e);
+        try (Connection conn = ds.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, pizzaId);
+            ps.setInt(2, ingredientId);
+            int ctrl = ps.executeUpdate();
+            if (ctrl != 1) throw new SQLException("could not add ingredient to pizza");
         } catch (TechnicalException | SQLException e) {
             throw new TechnicalException(e);
         }
@@ -75,8 +77,9 @@ public class pizzaDAO {
                 "LEFT JOIN ingredients ON pizza_ingredient.ingredient_id = ingredients.id " +
                 "GROUP BY p.label;";
         List<Pizza> pizzas = new ArrayList<>();
-        try (PreparedStatement stmt = ds.getConnection().prepareStatement(sql)) {
-            ResultSet rs = stmt.executeQuery();
+        try (Connection conn = ds.getConnection()) {
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            conn.close();
             while (rs.next()) {
                 Pizza pizza = new Pizza();
                 String ingredients = rs.getString("ingredients");
@@ -104,57 +107,4 @@ public class pizzaDAO {
         }
 
     }
-//    public static void addPizza(Pizza pizza) throws SQLException {
-//        String sql = "INSERT INTO pizza (label, ingredients, description, prix) VALUES (?,?,?,?);";
-//
-//        try (PreparedStatement stmt = ds.getConnection().prepareStatement(sql)) {
-//            stmt.setString(1, pizza.getLabel());
-//            stmt.setString(2, pizza.getIngredients());
-//            stmt.setString(3, pizza.getDescription());
-//            stmt.setDouble(4, pizza.getPrix());
-//            stmt.setInt(5, pizza.getCalories());
-//            stmt.executeUpdate(sql);
-//        } catch (SQLException e) {
-//            throw new SQLException(e);
-//        }
-//    }
-//    public List<Pizza> getList() throws SQLException {
-//        List<Pizza> list = new ArrayList<>();
-//
-//        String sqlSelect = "Select label, ingredients, descritpion, prix FROM articles";
-//        try (Statement stmt = ds.getConnection().createStatement();
-//             ResultSet rs = stmt.executeQuery(sqlSelect)) {
-//            pizzaBDLink(list, rs);
-//        } catch (SQLException e) {
-//            throw new SQLException(e);
-//        }
-//        return list;
-//    }
-//
-//    private void pizzaBDLink(List<Pizza> list, ResultSet rs) throws SQLException {
-//        while (rs.next()) {
-//            Pizza article = new Pizza();
-//            article.setId(rs.getInt(1));
-//            article.setLabel(rs.getString(2));
-//            article.setIngredients(rs.getDouble(3));
-//            article.setDescription(rs.getString(4));
-//            article.setPrix(rs.getInt(5));
-//            article.setCalories(rs.getInt(6));
-//            list.add(article);
-//        }
-//    }
-//
-//    public List<Pizza> getListByLabel(String label) throws SQLException {
-//        List<Pizza> list = new ArrayList<>();
-//
-//        String sql = "Select label, ingredients, descritpion, prix FROM articles WHERE label = '?'";
-//        try (PreparedStatement stmt = ds.getConnection().prepareStatement(sql)){
-//             stmt.setString(1, label);
-//             ResultSet rs = stmt.executeQuery();
-//            pizzaBDLink(list, rs);
-//        } catch (SQLException e) {
-//            throw new SQLException(e);
-//        }
-//        return list;
-//    }
 }
