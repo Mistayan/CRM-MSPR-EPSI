8d9c0b8047aad3fd814d41278926f7c9e8569845
Mistayan <Helixmastaz@gmail.com>
2022-06-05 00:20:48 +0200

    - voir les articles - changer / ajouter un article    - Pokemons    - plus à venir - désactiver un article

--- src/main/webapp/js/article.js
+++ src/main/webapp/js/article.js
@@ -4,33 +4,93 @@ const app = new Vue({
 
         return {
             articles: [],
-            ingredients: [],
+            categories: [],
+            properties: [],
             create: false,
-            newArticle: {},
-            newIngredient: {}
+            modify: false,
+            select: null,
+            newArticle: {
+                category: {id: -1},
+                properties: {id: -1},
+                stats: {id: -1}
+            },
+            newCat: [],
+            search: ""
         }
     },
-    mounted() { // Ce qui est affiché au chargement de la page
+    mounted() { // Ce qui est fait au chargement de la page
         axios.get("/admin/article")
             .then(response => {
                 this.articles = response.data.data;
             });
+        axios.get("/admin/categories")
+            .then(response => {
+                this.categories = response.data.data
+            });
+        // axios.get("/admin/properties")
+        //     .then(response => {
+        //         this.properties = response.data.data
+        //     });
     },
     methods: { // Methodes intéractives
 
         createArticle() {
-            axios.post('/admin/article/new_article', this.newArticle)
+            // concat
+            this.newArticle.category = {};
+            this.newArticle.category.id = this.newCat[0];
+            // console.log(this.newArticle)
+
+            axios.post('/admin/article/new', this.newArticle)
                 .then(response => {
                     if (response.data.success) {
-                        this.newArticle = {};
-                        this.newIngredient = {};
-                        this.create = false;
+                        this.newArticle = {
+                            category: {id: -1},
+                            properties:{id: -1},
+                            stats: {id: -1}
+                        };
+                        this.create = this.modify = this.select = false;
                         axios.get("/admin/article")
                             .then(response => {
                                 this.articles = response.data.data;
                             });
                     }
                 });
+        },
+        modifyArticle: function() {
+            console.log(this.newArticle)
+
+            axios.post('/admin/article/modify', this.newArticle)
+                .then(response => {
+                    if (response.data.success) {
+                        this.resetValues()
+                        this.create = this.modify = this.select = false;
+                        axios.get("/admin/article")
+                            .then(response => {
+                                this.articles = response.data.data;
+                            });
+                    }
+                });
+        },
+        removeArticle() {
+            console.log(this.newArticle)
+            axios.post('/admin/article/disable', this.newArticle)
+                .then(response => {
+                    if (response.data.success) {
+                        this.resetValues()
+                        this.create = this.modify = this.select = false;
+                        axios.get("/admin/article")
+                            .then(response => {
+                                this.articles = response.data.data;
+                            });
+                    }
+                });
+        },
+        resetValues(){
+            this.newArticle= {
+                category: {id: -1},
+                properties:{id: -1},
+                stats: {id: -1}
+            }
         }
     }
 });
