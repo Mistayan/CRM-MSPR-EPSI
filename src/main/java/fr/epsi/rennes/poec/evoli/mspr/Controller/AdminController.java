91f5d3a171292c485c7d92cb53f3e1ab587b4511
Mistayan <Helixmastaz@gmail.com>
2022-06-05 00:17:25 +0200

    Ajout d'un logger, ajout de 4 routes web: -changer un article -désactiver un article -ajouter un article -récupérer les différentes catégories d'articles

--- src/main/java/fr/epsi/rennes/poec/evoli/mspr/Controller/AdminController.java
+++ src/main/java/fr/epsi/rennes/poec/evoli/mspr/Controller/AdminController.java
@@ -1,13 +1,16 @@
 package fr.epsi.rennes.poec.evoli.mspr.Controller;
 
+import fr.epsi.rennes.poec.evoli.mspr.dao.ArticleCategory;
 import fr.epsi.rennes.poec.evoli.mspr.domain.Article;
 import fr.epsi.rennes.poec.evoli.mspr.domain.PokemonProperties;
 import fr.epsi.rennes.poec.evoli.mspr.domain.Response;
 import fr.epsi.rennes.poec.evoli.mspr.exception.BusinessException;
 import fr.epsi.rennes.poec.evoli.mspr.service.ArticleService;
 import fr.epsi.rennes.poec.evoli.mspr.service.PropertiesService;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,16 +27,14 @@ import java.util.List;
 
 @RestController
 public class AdminController {
-
+    private static final Logger logger = LogManager.getLogger(AdminController.class);
     private final ArticleService articleService;
     private final PropertiesService propertiesService;
-    private final Logger logger;
 
     @Autowired
-    public AdminController(ArticleService articleService, PropertiesService propertiesService, Logger logger) {
+    public AdminController(ArticleService articleService, PropertiesService propertiesService) {
         this.articleService = articleService;
         this.propertiesService = propertiesService;
-        this.logger = logger;
     }
 
     @GetMapping("/admin/properties")
@@ -58,13 +59,55 @@ public class AdminController {
         return response;
     }
 
-    @PostMapping("/admin/article/new_article")
-    public Response<Void> newArticle(@RequestBody Article article) throws BusinessException {
-        if (article == null) {
+    @PostMapping("/admin/article/new")
+    public Response<Void> newArticle(@RequestBody Article newArticle) throws BusinessException {
+        if (newArticle == null) {
             throw new BusinessException("newArticle.param.null");
         }
-        articleService.createArticle(article);
-        return new Response<>();
+        articleService.createArticle(newArticle);
+        Response<Void> ret = new Response<>();
+        ret.setSuccess(true);
+        return ret;
     }
 
+    @GetMapping("/admin/categories")
+    public Response<List<ArticleCategory>> getArticlesCategories() throws BusinessException {
+        Response<List<ArticleCategory>> response = new Response<>();
+        List<ArticleCategory> categories = articleService.getAllCategories();
+        if (categories == null) {
+            response.setSuccess(false);
+            throw new BusinessException("getArticlesService : : : could not retrieve categories");
+        }
+        response.setData(categories);
+        response.setSuccess(true);
+        return response;
+    }
+
+    @PostMapping("/admin/article/disable")
+    public Response<Void> removeArticle(@RequestBody Article newArticle) throws BusinessException {
+        if (newArticle == null) {
+            throw new BusinessException("newArticle.param.null");
+        }
+        articleService.removeArticle(newArticle.getId());
+        Response<Void> ret = new Response<>();
+        ret.setSuccess(true);
+        return ret;
+    }
+
+    @PostMapping("/admin/article/modify")
+    public Response<Void> modifyArticle(@RequestBody Article newArticle) throws BusinessException {
+        Response<Void> ret = new Response<>();
+        if (newArticle == null) {
+            throw new BusinessException("newArticle.param.null");
+        }
+        logger.trace("Admin/article/modify");
+        try {
+            articleService.modifyArticle(newArticle);
+            ret.setSuccess(true);
+        } catch (BusinessException e) {
+            ret.setSuccess(false);
+            throw new BusinessException("newArticle.modify.failed");
+        }
+        return ret;
+    }
 }
