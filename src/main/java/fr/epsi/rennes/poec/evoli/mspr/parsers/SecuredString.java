7c0a3d43c486df9324e00542a2c4bb934a42edfe
Mistayan <Helixmastaz@gmail.com>
2022-06-15 14:35:33 +0200

    template securedStrings

---
+++ src/main/java/fr/epsi/rennes/poec/evoli/mspr/parsers/SecuredString.java
@@ -0,0 +1,18 @@
+package fr.epsi.rennes.poec.evoli.mspr.parsers;
+
+/**
+ * Author : Stephen Mistayan
+ * Created on : 6/12/2022 : 1:01 AM:30
+ * IDE : IntelliJ IDEA
+ * Original package : fr.epsi.rennes.poec.evoli.mspr.parsers
+ * Project name : acme MSPR
+ **/
+
+public class SecuredString {
+    final String securedString;
+
+    public SecuredString(String securedString) {
+        
+        this.securedString = securedString;
+    }
+}
