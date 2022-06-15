6ed66ee877b97c0899b7cf0d424de987a92c856f
Mistayan <Helixmastaz@gmail.com>
2022-06-15 14:18:25 +0200

    Front

--- src/main/webapp/js/customer.js
+++ src/main/webapp/js/customer.js
@@ -26,7 +26,6 @@ const customerApp = new Vue({
         //     });
     },
     methods: { // Methodes intéractives
-
         createCustomer: function () {
             console.log(this.newCustomer)
 
@@ -73,13 +72,14 @@ const customerApp = new Vue({
             }
         },
         filterListCustomers(customer) {
-            let fn = customer.firstName.toLowerCase().includes(this.search.toLowerCase())
-            let ln = customer.lastName.toLowerCase().includes(this.search.toLowerCase())
-            let ma = customer.email ? customer.email.toLowerCase().includes(this.search.toLowerCase()) : null
-            let cn = customer.contactNumber ? customer.contactNumber.toLowerCase().includes(this.search.toLowerCase()) : null
-            let ci = customer.address.city ? customer.address.city.toLowerCase().includes(this.search.toLowerCase()) : null
-            let co = customer.address.country ? customer.address.country.toLowerCase().includes(this.search.toLowerCase()) : null
-            return fn ? fn : ln ? ln : ma ? ma : cn ? cn : ci ? ci : co;
+            let fn = customer.firstName.toLowerCase().includes(this.search.toString().toLowerCase())
+            let ln = customer.lastName.toLowerCase().includes(this.search.toString().toLowerCase())
+            let ma = customer.email ? customer.email.toLowerCase().includes(this.search.toString().toLowerCase()) : null
+            let cn = customer.contactNumber ? customer.contactNumber.toString().includes(this.search.toString()) : null
+            let ci = customer.address.city ? customer.address.city.toLowerCase().includes(this.search.toString().toLowerCase()) : null
+            let co = customer.address.country ? customer.address.country.toLowerCase().includes(this.search.toString().toLowerCase()) : null
+            let cp = customer.address.postalCode ? customer.address.postalCode.toString().includes(this.search.toString()) : null
+            return fn ? fn : ln ? ln : ma ? ma : cn ? cn : ci ? ci : co ? co : cp;
         }
     }
 });
