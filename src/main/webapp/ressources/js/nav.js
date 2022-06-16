54fe601eb4a2ed6c88c8672d94aa3744f934059b
Mistayan <Helixmastaz@gmail.com>
2022-06-16 21:02:27 +0200

    Prototype page dynamique

--- src/main/webapp/ressources/js/nav.js
+++ src/main/webapp/ressources/js/nav.js
@@ -1,13 +1,20 @@
 const nav = new Vue({
-    el: '#nav',
+    el: '#head',
     mounted() {
+        // Si une redirection était en cours, elle se poursuivra
         if (window.sessionStorage.getItem('nav')) {
             let link = window.sessionStorage.getItem('nav');
             sessionStorage.removeItem('nav')
             window.location.replace(link)
-        } else {
-            let link = "/index.html";
-            window.location.replace(link)
         }
+        axios.get("/public/auth")
+            .then(response => {
+                if (!response.data.success || !response.data.data) {
+                    return
+                }
+                document.body.insertAdjacentHTML("#head", `
+    
+`)
+            })
     }
 });
