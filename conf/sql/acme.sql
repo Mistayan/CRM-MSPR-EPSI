7d97f3064d0a6b28deb04560b773237e0a693181
Mistayan <Helixmastaz@gmail.com>
2022-06-16 15:57:53 +0200

    updated Prod pom.xml acme.sql, minor changes RUN-ME.bat => acmapp-init

--- conf/sql/acme.sql
+++ conf/sql/acme.sql
@@ -6,15 +6,15 @@ SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
         'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema test_acme
+-- Schema test2_acme
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `test_acme`;
+DROP SCHEMA IF EXISTS `test2_acme`;
 
 -- -----------------------------------------------------
 -- Schema test_acme
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `test_acme` DEFAULT CHARACTER SET utf8;
-USE `test_acme`;
+CREATE SCHEMA IF NOT EXISTS `test2_acme` DEFAULT CHARACTER SET utf8;
+USE `test2_acme`;
 
 -- -----------------------------------------------------
 -- Table `category`
