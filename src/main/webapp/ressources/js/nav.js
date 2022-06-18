6b539361bf6d94efc4560d413b56cc0b1d1d6e33
Mistayan <Helixmastaz@gmail.com>
2022-06-18 17:09:09 +0200

    meilleur navigation, et vérification d'authentification / auto-redirect

--- src/main/webapp/ressources/js/nav.js
+++ src/main/webapp/ressources/js/nav.js
@@ -1,20 +1,43 @@
 const nav = new Vue({
-    el: '#head',
+    alt: '#nav-item',
+    el: '#nav',
+    data() { //le modèle de données
+        return {
+            auth: null,
+            link: null,
+            select: false
+        }
+    },
     mounted() {
         // Si une redirection était en cours, elle se poursuivra
         if (window.sessionStorage.getItem('nav')) {
-            let link = window.sessionStorage.getItem('nav');
-            sessionStorage.removeItem('nav')
-            window.location.replace(link)
+            this.link = window.sessionStorage.getItem('nav');
+            console.log('test')
+            if (this.link.contains('index.html') || this.link === '/' || this.link === "/#") {
+                console.log('test')
+                window.sessionStorage.clear()
+            }
+            if (window.sessionStorage.getItem('last_status') === 'fail') {
+                console.log('move')
+                window.sessionStorage.removeItem('last_status')
+                window.location.replace(this.link)
+            }
+        } else {
+            window.sessionStorage.setItem('nav', window.location.href)
         }
         axios.get("/public/auth")
             .then(response => {
                 if (!response.data.success || !response.data.data) {
-                    return
+                    // window.location.replace("/login")
+                    window.alert("...")
                 }
-                document.body.insertAdjacentHTML("#head", `
-    
-`)
             })
+    },
+    methods: {
+        redirect: function () {
+            this.link = window.sessionStorage.getItem('nav');
+            window.location.replace(this.link)
+
+        }
     }
 });
